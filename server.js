3a881b9d2a830d1fc1bfba2888ee86f0c2c26a43
NeetuMaheshwari <neetumahe@gmail.com>
2021-07-22 17:14:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 
 const app = express()
-const http = require('https').createServer(app)
+const http = require('http').createServer(app)
 const PORT = process.env.PORT || 3000
 
 http.listen(PORT, ()=>{console.log(`listen on PORT ${PORT}`)})
 
 app.use(express.static(__dirname+'/public'))
 
-
+app.get('/',(req,res) => {res.sendFile(__dirname +'/index.html')})
 
 //socket.io
 
