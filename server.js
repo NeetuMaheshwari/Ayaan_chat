591d800fa56955cb4e7949579f2fc5d8548e2e05
NeetuMaheshwari <neetumahe@gmail.com>
2021-07-22 16:29:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express()
-const http = require('http').createServer(app)
+const http = require('https').createServer(app)
 const PORT = process.env.PORT || 3000
 
 http.listen(PORT, ()=>{console.log(`listen on PORT ${PORT}`)})
